c7c64753e8686c555a5e7e7be9803c10a8ceffee
amaniummadi <amaniummadi123@gmail.com>
2020-10-07 08:50:07 +0530

    Updates

---
+++ precache-manifest.53db4fe0e6ed9fb7e65d4ebe0467c261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdb4a9a4e41cc48f79df9fe808563c8",
+    "url": "/Amaniummadi/gitHubURl/index.html"
+  },
+  {
+    "revision": "86df246b9c2796ddb464",
+    "url": "/Amaniummadi/gitHubURl/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9638341745338c02a778",
+    "url": "/Amaniummadi/gitHubURl/static/js/2.9203d5fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Amaniummadi/gitHubURl/static/js/2.9203d5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86df246b9c2796ddb464",
+    "url": "/Amaniummadi/gitHubURl/static/js/main.8dc56262.chunk.js"
+  },
+  {
+    "revision": "515f8561624ee48163fe",
+    "url": "/Amaniummadi/gitHubURl/static/js/runtime-main.c58e21b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Amaniummadi/gitHubURl/static/media/logo.5d5d9eef.svg"
+  }
+]);
